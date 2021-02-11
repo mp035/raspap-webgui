b3d68af064b153e66fe478d32237c987866e6f5a
Mark Pointing <mark@futurepoint.com.au>
2021-02-11 18:19:37 +1000

    Got UI for viewing current networks in place with display of hidden in status line.

--- includes/configure_client.php
+++ includes/configure_client.php
@@ -33,7 +33,9 @@ function DisplayWPAConfig()
                     $tmp_networks[$_POST['ssid' . $post_match[1]]] = array(
                     'protocol' => ( $_POST['protocol' . $post_match[1]] === 'Open' ? 'Open' : 'WPA' ),
                     'passphrase' => $_POST['passphrase' . $post_match[1]],
-                    'configured' => true
+                    'configured' => true,
+                    'hidden'=> ($_POST['hidden' . $post_match[1]] ?? "0") == "1",
+
                     );
                     if (array_key_exists('priority' . $post_match[1], $_POST)) {
                         $tmp_networks[$_POST['ssid' . $post_match[1]]]['priority'] = $_POST['priority' . $post_match[1]];
@@ -47,7 +49,9 @@ function DisplayWPAConfig()
                     fwrite($wpa_file, "network={".PHP_EOL);
                     fwrite($wpa_file, "\tssid=\"".$ssid."\"".PHP_EOL);
                     fwrite($wpa_file, "\tkey_mgmt=NONE".PHP_EOL);
-                    fwrite($wpa_file, "\tscan_ssid=1".PHP_EOL);
+                    if($network['hidden']){
+                        fwrite($wpa_file, "\tscan_ssid=1".PHP_EOL);
+                    }
                     if (array_key_exists('priority', $network)) {
                         fwrite($wpa_file, "\tpriority=".$network['priority'].PHP_EOL);
                     }
@@ -62,6 +66,9 @@ function DisplayWPAConfig()
                                 if (array_key_exists('priority', $network)) {
                                     fwrite($wpa_file, "\tpriority=".$network['priority'].PHP_EOL);
                                 }
+                                if($network['hidden']){
+                                    fwrite($wpa_file, "\tscan_ssid=1".PHP_EOL);
+                                }
                                 fwrite($wpa_file, $line.PHP_EOL);
                             } else {
                                 fwrite($wpa_file, $line.PHP_EOL);
@@ -73,7 +80,6 @@ function DisplayWPAConfig()
                     }
                 }
             }
-
             if ($ok) {
                 system('sudo cp /tmp/wifidata ' . RASPI_WPA_SUPPLICANT_CONFIG, $returnval);
                 if ($returnval == 0) {
--- views/configure_client.blade.php
+++ views/configure_client.blade.php
@@ -23,6 +23,11 @@
             <h4 class="mb-2">{{ _("Client settings") }}</h4>
           </div>
           <div class="col-xs mr-3 mb-3">
+            <button type="button" class="btn btn-info btn-block float-right" data-toggle="modal" data-target="#hiddenClientModal">
+              {{_("Connect to Hidden Wifi Network")}}
+            </button>
+          </div>
+          <div class="col-xs mr-3 mb-3">
             <button type="button" class="btn btn-info btn-block float-right js-reload-wifi-stations">{{ _("Rescan") }}</button>
           </div>
         </div>
@@ -30,6 +35,25 @@
             {!! CSRFTokenFieldTag() !!}
           <input type="hidden" name="client_settings" ?>
           <div class="row js-wifi-stations w-100 loading-spinner"></div>
+
+          <!-- Modal -->
+          <div class="modal fade" id="hiddenClientModal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <div class="modal-title" id="ModalLabel"><i class="fas fa-sync-alt mr-2"></i> {{_("Connect to Hidden Wifi Network")}} </div>
+                </div>
+                <div class="modal-body">
+                  <p>This is where the form will go.</p>
+                </div>
+                <div class="modal-footer">
+                  <input type="submit" class="btn btn-info" value="{{ _("Connect") }}" id="connectHidden" name="connectHidden"/>
+                  <button type="button" class="btn btn-outline btn-primary" data-dismiss="modal">{{ _("Cancel") }}</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </form>
       </div><!-- ./ card-body -->
       <div class="card-footer">{!! _("<strong>Note:</strong> WEP access points appear as 'Open'. RaspAP does not currently support connecting to WEP") !!}</div>
